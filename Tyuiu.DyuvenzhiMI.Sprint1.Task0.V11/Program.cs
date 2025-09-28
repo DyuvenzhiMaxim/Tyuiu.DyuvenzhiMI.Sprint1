e9b445936eaa7ae4943aae4305d01193710b7619
DyuvenzhiMaxim <Tyuiu.DyuvenzhiMI@outlook.com>
2025-09-29 00:55:26 +0500

    Add project files.

---
+++ Tyuiu.DyuvenzhiMI.Sprint1.Task0.V11/Program.cs
@@ -0,0 +1,39 @@
+﻿
+// Задание
+// Написать программу, которая вычисляет выражение 4 * 5 / 2 - 18 / 2 / 3 и выводит результат на экране.
+
+using Tyuiu.DyuvenzhiMI.Sprint1.Task0.V11.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнил Дювенжи М. И. | ПИНТб-25-1";
+        //Длина строки 75 символов
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #11                                                              *");
+        Console.WriteLine("* Выполнил: Дювенжи Максим Иванович | ПИНТб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет выражение 4 * 5 / 2 - 18 / 2 / 3  *");
+        Console.WriteLine("* и печатает результат на экране.                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* 4 * 5 / 2 - 18 / 2 / 3                                                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        //Метож Calculate находится в библиотеке Tyuiu.DyuvenzhiMI.Sprint1.Task0.V11.Lib
+        //в классе DataService
+        Console.WriteLine(ds.Calculate());
+
+        Console.ReadKey();
+    }
+}
