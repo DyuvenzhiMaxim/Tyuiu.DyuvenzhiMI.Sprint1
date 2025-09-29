5c0d68bb195b95824a51dbf83afe735a4b45dc34
DyuvenzhiMaxim <Tyuiu.DyuvenzhiMI@outlook.com>
2025-09-29 17:15:00 +0500

    Task2

---
+++ Tyuiu.DyuvenzhiMI.Sprint1.Task2.V3.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿
+using Tyuiu.DyuvenzhiMI.Sprint1.Task2.V3.Lib;
+namespace Tyuiu.DyuvenzhiMI.Sprint1.Task2.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            var res = ds.ConvertHourToMin(x);
+            Assert.AreEqual(x*60, res);
+        }
+    }
+}
---
+++ Tyuiu.DyuvenzhiMI.Sprint1.Task2.V3/Program.cs
@@ -0,0 +1,49 @@
+﻿
+//ЗАДАНИЕ
+//Написать программу, которая запрашивает у пользователя исходные данные,
+//выполняет указанные расчёты и печаетает результат на экране.
+//Расчёты: Известно часы.Найти сколько это в минутах.
+//Количество часов (целое число)
+// Количество часов * 60 (целое число)
+
+using Tyuiu.DyuvenzhiMI.Sprint1.Task2.V3.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнил: Дювенжи М. И. | ПИНТб-25-1";
+        //Длина строки 75 символов
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #3                                                              *");
+        Console.WriteLine("* Выполнил: Дювенжи Максим Иванович | ПИНТб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x;
+        Console.WriteLine("Время в часах:");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        //Метод ConvertHourToMin находится в библиотеке Tyuiu.DyuvenzhiMI.Sprint1.Task2.V3.Lib
+        //в классе DataService
+        Console.WriteLine("Время в минутах:");
+        Console.WriteLine(ds.ConvertHourToMin(x));
+
+        Console.ReadLine();
+
+    }
+}
